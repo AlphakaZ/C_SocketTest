7fb1db7ca1b93ddc0af1f3bb9b5935580f151c8b
Saito Kazuto <saitokazuto@Saito-no-MacBook-Air.local>
2018-05-10 08:23:32 +0900

    分割ファイル

--- src/tcpd_dv.c
+++ src/tcpd_dv.c
@@ -1,5 +1,4 @@
 // 参考・引用：https://qiita.com/tajima_taso/items/2f0606db7764580cf295
-
 #include <stdio.h> //printf(), fprintf(), perror()
 #include <sys/socket.h> //socket(), bind(), accept(), listen()
 #include <arpa/inet.h> // struct sockaddr_in, struct sockaddr, inet_ntoa()
@@ -19,8 +18,7 @@ void l2b(char* str){
     }
 }
 
-int main(int argc, char* argv[]) {
-
+typedef struct{
     int servSock; //server socket descripter
     int clitSock; //client socket descripter
     struct sockaddr_in servSockAddr; //server internet socket address
@@ -29,38 +27,81 @@ int main(int argc, char* argv[]) {
     unsigned int clitLen; // client internet socket address length
     char recvBuffer[BUFSIZE];
     int recvMsgSize, sendMsgSize;
+}ServerModule;
 
+void initServer(ServerModule *s){
+    //何もしない
+}
+
+void argCheck(int argc, char** argv){
     if ( argc != 2) {//引数のチェック
         fprintf(stderr, "argument count mismatch error.\n");
         exit(EXIT_FAILURE);
     }
-    // 文字列になっているポート番号を整数に変換する
-    if ((servPort = (unsigned short) atoi(argv[1])) == 0) {
+}
+
+int getServPortNumber(char* str)
+{
+    int n = (unsigned short) atoi(str);
+    if(n == 0){
         fprintf(stderr, "invalid port number.\n");
-        exit(EXIT_FAILURE);
+        exit(EXIT_FAILURE);        
     }
-    // socket(プロトコルファミリ,  ソケットの種類, プロトコル)は、OSにソケットの作成を依頼するシステムコール
-    if ((servSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0 ){
+    return n;
+}
+
+int genServSocket()
+{
+    int n = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if(n < 0){
         perror("socket() failed.");
         exit(EXIT_FAILURE);
     }
-    //サーバ側の構造体を初期化
-    memset(&servSockAddr, 0, sizeof(servSockAddr));
-    servSockAddr.sin_family      = AF_INET;// AF_INET = IPv4
-    servSockAddr.sin_addr.s_addr = htonl(INADDR_ANY);// 
-    servSockAddr.sin_port        = htons(servPort);//ポート番号の格納 htonl(),htons()はネットワークに対応するエンディアン変換関数
-    
-    //作成したソケットにIPアドレスとポート番号を紐づける
-    if (bind(servSock, (struct sockaddr *) &servSockAddr, sizeof(servSockAddr) ) < 0 ) {
+    return n;
+}
+
+void initServSockAddr(ServerModule* sMdl){
+    memset(&(sMdl->servSockAddr),0,sizeof(sMdl->servSockAddr));
+    sMdl->servSockAddr.sin_family      = AF_INET;// AF_INET = IPv4
+    sMdl->servSockAddr.sin_addr.s_addr = htonl(INADDR_ANY);// 
+    sMdl->servSockAddr.sin_port        = htons(sMdl->servPort);//ポート番号の格納 htonl(),htons()はネットワークに対応するエンディアン変換関数
+}
+
+void bindSocketWithAddress(ServerModule* sMdl){
+    if (bind(sMdl->servSock, (struct sockaddr *) &(sMdl->servSockAddr), sizeof(sMdl->servSockAddr) ) < 0 ) {
         perror("bind() failed.");
         exit(EXIT_FAILURE);
     }
-    //ここで初めて、クライアントからの接続状態を受け付ける状態になる
-    if (listen(servSock, QUEUELIMIT) < 0) {
+}
+
+void readyServer(ServerModule* sMdl){
+    if (listen(sMdl->servSock, QUEUELIMIT) < 0) {
         perror("listen() failed.");
         exit(EXIT_FAILURE);
     }
+}
 
+void setupServer(int argc, char** argv,ServerModule* sMdl){
+    initServer(sMdl);
+    argCheck(argc,argv);
+
+    // 文字列になっているポート番号を整数に変換する
+    sMdl.servPort = getServPortNumber(argv[1]);
+    // socket(プロトコルファミリ,  ソケットの種類, プロトコル)は、OSにソケットの作成を依頼するシステムコール
+    sMdl.servSock = genServSocket();
+
+    //サーバ側の構造体を初期化
+    initServSockAddr(sMdl);
+    
+    //作成したソケットにIPアドレスとポート番号を紐づける
+    bindSocketWithAddress(sMdl);
+
+    //ここで初めて、クライアントからの接続状態を受け付ける状態になる
+    readyServer(sMdl);
+}
+
+void runserver(ServerModule* module,)
+{
     while(1) {
         clitLen = sizeof(clitSockAddr);
         if ((clitSock = accept(servSock, (struct sockaddr *) &clitSockAddr, &clitLen)) < 0) {
@@ -69,6 +110,7 @@ int main(int argc, char* argv[]) {
         }
 
         printf("connected from %s.\n", inet_ntoa(clitSockAddr.sin_addr));
+
         while(1) {
             if ((recvMsgSize = recv(clitSock, recvBuffer, BUFSIZE, 0)) < 0) {
                 perror("recv() failed.");
@@ -77,13 +119,11 @@ int main(int argc, char* argv[]) {
                 fprintf(stderr, "connection closed by foreign host.\n");
                 break;
             }
-            // printf("Recv: %s",recvBuffer);
-            // l2b(recvBuffer);
+
             strcpy(recvBuffer,"Return\n");//固定文字列
             recvMsgSize = strlen("Return\n");
 
             if((sendMsgSize = send(clitSock, recvBuffer, recvMsgSize, 0)) < 0){
-            // if((sendMsgSize = send(clitSock,message,,0)) < 0){
                 perror("send() failed.");
                 exit(EXIT_FAILURE);
             } else if(sendMsgSize == 0){
@@ -93,6 +133,15 @@ int main(int argc, char* argv[]) {
         }
         close(clitSock);
     }
+}
+
+int main(int argc, char* argv[]) {
+
+    ServerModule sMdl;
+
+    setupServer(&sMdl);
+
+    
 
     close(servSock);
 
